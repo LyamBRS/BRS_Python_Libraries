fc469742d78e3b62cef56282999700c40ca85db6
LyamBRS <=>
2023-05-19 11:22:11 -0400

    Added GetPrimaryColor and GetAppTheme

--- BRS/GUI/Utilities/colors.py
+++ BRS/GUI/Utilities/colors.py
@@ -30,6 +30,25 @@ def GetAccentColor(variant:str = "500") -> None:
         This function returns the accent color
         of the application.
 
+        Arguments:
+        ----------
+        - "50"
+        - "100"
+        - "200"
+        - "300"
+        - "400"
+        - "500" Default value
+        - "600"
+        - "700"
+        - "800"
+        - "900"
+        - "A100"
+        - "A200"
+        - "A400"
+        - "A700"
+        - "Dark"
+        - "Light"
+
         Returns:
         --------
         - (0,0,0) to (1,1,1)
@@ -37,8 +56,19 @@ def GetAccentColor(variant:str = "500") -> None:
     Debug.Start("GetAccentColor")
 
     try:
-        color = MDApp.get_running_app().theme_cls.accent_palette
-        color = get_color_from_hex(colors[color][variant])
+        if(variant == "Dark"):
+            color = MDApp.get_running_app().theme_cls.accent_dark
+            Debug.Log("Returning darker color")
+            Debug.End()
+            return color
+        elif(variant == "Light"):
+            color = MDApp.get_running_app().theme_cls.accent_light
+            Debug.Log("Returning lighter color")
+            Debug.End()
+            return color
+        else:
+            color = MDApp.get_running_app().theme_cls.accent_palette
+            color = get_color_from_hex(colors[color][variant])
     except:
         Debug.Error("Failed to get accent color.")
         Debug.Log("Returning red")
@@ -46,6 +76,81 @@ def GetAccentColor(variant:str = "500") -> None:
     Debug.End()
     return color
 
+def GetPrimaryColor(variant:str = "500") -> None:
+    """
+        GetPrimaryColor:
+        ===============
+        Summary:
+        --------
+        This function returns the primary color
+        of the application.
+
+        Arguments:
+        ----------
+        - "50"
+        - "100"
+        - "200"
+        - "300"
+        - "400"
+        - "500" Default value
+        - "600"
+        - "700"
+        - "800"
+        - "900"
+        - "A100"
+        - "A200"
+        - "A400"
+        - "A700"
+        - "Dark"
+        - "Light"
+
+        Returns:
+        --------
+        - (0,0,0) to (1,1,1)
+    """
+    Debug.Start("GetAccentColor")
+
+    try:
+        if(variant == "Dark"):
+            color = MDApp.get_running_app().theme_cls.primary_dark
+            Debug.Log("Returning darker color")
+            Debug.End()
+            return color
+        elif(variant == "Light"):
+            color = MDApp.get_running_app().theme_cls.primary_light
+            Debug.Log("Returning lighter color")
+            Debug.End()
+            return color
+        else:
+            color = MDApp.get_running_app().theme_cls.primary_palette
+            color = get_color_from_hex(colors[color][variant])
+    except:
+        Debug.Error("Failed to get primary color.")
+        Debug.Log("Returning red")
+        color = (1,0,0)
+    Debug.End()
+    return color
+
+def GetAppTheme() -> str:
+    """
+        GetAppTheme:
+        ============
+        Summary:
+        --------
+        Returns either "Light"
+        or "Dark"
+    """
+    Debug.Start("GetAppTheme")
+    try:
+        color = MDApp.get_running_app().theme_cls.theme_style
+        Debug.End()
+        return color
+    except:
+        Debug.Error("Failed to get app theme.")
+        Debug.Log("Returning red")
+        color = (1,0,0)
+    Debug.End()
+    return "Error"
 #====================================================================#
 # Classes
 #====================================================================#
